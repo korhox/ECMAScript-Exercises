d84a6c25af5b9cbdc8bc5f7da61681ddf91d0886
korhox <juuso@korho.fi>
2022-09-06 20:44:22 +0300

    Add e15

--- homework/01/03-13/15.js
+++ homework/01/03-13/15.js
@@ -1,6 +1,5 @@
-
 // How many tries we do
-const maxTries = 500000000000000;
+const maxTries = 100000000000;
 
 
 function generateLotto() {
@@ -8,30 +7,32 @@ function generateLotto() {
     for (let j = 0; j < 7; j++) {
         lotto.push(Math.floor(Math.random() * 40) + 1);
     }
+    lotto.sort(function (a, b) { return a - b });
     return lotto;
 }
 
-function isWin(array1, array2) {
-    let isWin = true;
-    for (let i = 0; i < array2.length; i++) {
-        if (!array2.includes(array1[i])) {
-            isWin = false;
+function winNumbers(guess, lotto) {
+    var rightOnes = 7;
+    for (let i = 0; i < lotto.length; i++) {
+        if (!lotto.includes(guess[i])) {
+            rightOnes -= 1;
         }
     }
-    return isWin;
+    return rightOnes;
 }
 
 function parseVars(vars) {
     for (let i = 0; i < vars.length; i++) {
         vars[i] = Number(vars[i]);
     }
+    vars.sort(function (a, b) { return a - b });
     return vars;
 }
 
 function main() {
     let guess = parseVars(process.argv.slice(2));
-    let wins = 0;
     let startTime = Date.now();
+    let right = 0;
 
     if (guess.length != 7) {
         console.log("Please pass 7 numbers as arguments.")
@@ -39,14 +40,29 @@ function main() {
     }
 
     for (let i = 0; i < maxTries; i++) {
-        let lotto = generateLotto();
-        lotto.sort(function (a, b) { return a - b });
-        if (isWin(guess, lotto)) {
-            wins++;
+        var lotto = generateLotto();
+        let rightOnes = winNumbers(guess, lotto);
+        let tries = Math.round(i + 1);
+        if (rightOnes > right) {
+            right = rightOnes;
             let elapsedTime = Date.now() - startTime;
-            console.log(`A match found! You got ${wins} right so far. It's been ${Math.round((i + 1 / 365) / 1000)} millennia though. It's been ${Math.round(elapsedTime / 1000)} seconds since start of the script.`)
+            let timer = "";
+            if (elapsedTime > 5000 || right > 5) {
+                timer = ` It's been ${Math.round(elapsedTime / 1000)} seconds since start of the script.`;
+            }
+            if (tries >= 52) {
+                tries = Math.round(tries / 52) + " years";
+            } else {
+                tries = tries + " weeks";
+            }
+            console.log(`A match found! You got ${right} numbers right so far. It's been ${tries} though.` + timer)
+        }
+        if (right >= 7) {
+            console.log("Done!");
+            process.exit(0);
         }
     }
+    console.log("> We run out of tries. Please increase the maxTries to run guesser longer.");
 }
 
 main();
