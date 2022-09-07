a5e333e440b00d8797f21ad397a9afac7959c028
korhox <juuso@korho.fi>
2022-09-07 22:19:10 +0300

    Add 04

---
+++ homework/02/01-10/04.js
@@ -0,0 +1,37 @@
+const isPositiveInteger = (value, onSuccess, onError) => { value >= 0 ? onSuccess() : onError(); }
+
+isPositiveInteger(5, () => {
+    console.log("Yeah!");
+}, () => {
+    console.log("Nope.");
+});
+
+isPositiveInteger(0, () => {
+    console.log("Yeah!");
+}, () => {
+    console.log("Nope.");
+});
+
+isPositiveInteger(-0.000000000000000000000000000000000000000000000000000001, () => {
+    console.log("Yeah!");
+}, () => {
+    console.log("Nope.");
+});
+
+isPositiveInteger(-5, () => {
+    console.log("Yeah!");
+}, () => {
+    console.log("Nope.");
+});
+
+isPositiveInteger(Infinity, () => {
+    console.log("Yeah!");
+}, () => {
+    console.log("Nope.");
+});
+
+isPositiveInteger(-Infinity, () => {
+    console.log("Yeah!");
+}, () => {
+    console.log("Nope.");
+});
