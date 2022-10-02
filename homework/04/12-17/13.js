9debd95daa5565035165898fa4bbdc568d6d81a7
korhox <juuso@korho.fi>
2022-10-02 23:32:20 +0300

    add 13

---
+++ homework/04/12-17/13.js
@@ -0,0 +1,21 @@
+{
+    function doIt() {
+        console.log(this)
+    }
+    doIt()
+
+
+    let x = doIt
+    x()
+}
+{
+    function doIt() {
+        console.log(this)
+    }
+    obj = { "key": "value" }
+
+    // Create a copy of the doIt() function so that
+    // the keyword thisis replaced with obj
+    let x = doIt.bind(obj)
+    x()
+}
