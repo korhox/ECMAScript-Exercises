7c0c8679a9f85b818589f3e39e04b386314cd864
korhox <juuso@korho.fi>
2022-10-03 01:51:41 +0300

    add 6

---
+++ homework/05/05-10/06.js
@@ -0,0 +1,37 @@
+// class Person {
+//     constructor(name) {
+//         this.name = name
+//     }
+//     drinkBeer() {
+//         console.log(this.name + " drinks beer")
+//     }
+//     sayHello() {
+//         console.log('hello from ' + this.name)
+//     }
+// }
+// let jack = new Person('jack')
+// jack.sayHello()
+// let tina = new Person('tina')
+// tina.sayHello()
+
+// Person.prototype.sayHello()
+// Person.prototype.drinkBeer()
+
+
+
+class Person {
+    constructor(name) {
+        this.name = name
+        this.drinkBeer = function () {
+            console.log(this.name + " drinks beer")
+        }
+    }
+    sayHello() {
+        console.log('hello from ' + this.name)
+    }
+}
+
+let jack = new Person('jack')
+jack.sayHello()
+let tina = new Person('tina')
+tina.sayHello()
